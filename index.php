70c6f7b5948b9516a11432c6a704a8e1dee72c52
Tawose oluwatimilehin <oluwatimilehintawose2005@gmail.com>
2025-03-14 16:47:58 +0100

    intial commit

--- index.php
+++ index.php
@@ -1,16 +1,49 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="assets/css/style.css">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        crossorigin="anonymous">
     <title>FrontEnd development - Learnpod academy</title>
 </head>
+
 <body>
-    <body class="bg-primary p-3">
-        <p class="text-center text-white">Register now and get 50% last-minute offer</p>
+
+    <body>
+        <div class="bg-primary p-2 z-3">
+            <p class="text-center text-white fw-bold fs-5">Register now and get 50% last-minute offer</p>
+        </div>
+        <div class="bg-black p-2">
+
+            <div class="container">
+                <div class="hero mt-3">
+                    <div class="logo">
+                        <a href=""><img src="assets/images/LearnPOD-logo-300x55.png" alt=""></a>
+                    </div>
+                    <div class="hero-text mt-2">
+                        <h5 class="text-white fw-normal mx-3">Ready to elevate your skills?</h5>
+
+                        <h2 class=" mx-3 fw-bold">
+                            Join our
+                        </h2>
+                        <h1 class="mx-3 fw-bold">
+                            Frontend <br>
+                            Development.
+                        </h1>
+                        <h2 class="mx-3 fw-bold">
+                            Live Classes
+
+                        </h2>
+                    </div>
+                </div>
+            </div>
+        </div>
     </body>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.3/js/bootstrap.min.js" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.3/js/bootstrap.min.js" crossorigin="anonymous"
+        referrerpolicy="no-referrer"></script>
 </body>
+
 </html>
